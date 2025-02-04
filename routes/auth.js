bdbb0065cdc08c50a0e9b315d9b684793011f378
Wahid Shaikh <wahid.shaikh@dynacommerce.com>
2025-02-04 13:00:54 +0530

    Add Logout + Profile edit APIs

--- routes/auth.js
+++ routes/auth.js
@@ -54,4 +54,12 @@ authRouter.post("/login", async (req, res) => {
   }
 });
 
+authRouter.post("/logout", async (req, res) => {
+  res
+    .cookie("token", null, {
+      expires: new Date(Date.now()),
+    })
+    .send("Logout successful!!!");
+});
+
 module.exports = authRouter;
